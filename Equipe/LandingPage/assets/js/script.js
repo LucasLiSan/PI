47bbf6e0e752fb9db6a84ecbdce00d5aafb4c0fe
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-04-03 17:44:42 -0300

    landingpage

---
+++ Equipe/LandingPage/assets/js/script.js
@@ -0,0 +1,13 @@
+function change() {
+    var quemSomos = document.getElementById("Qs");
+    var valores = document.getElementById("V");
+    quemSomos.style.display = "grid";
+    valores.style.display = "none";
+}
+
+function changeV() {
+    var quemSomos = document.getElementById("Qs");
+    var valores = document.getElementById("V");
+    quemSomos.style.display = "none";
+    valores.style.display = "grid";
+}

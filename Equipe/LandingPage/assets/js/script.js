3d36468733a12d09a6e31314bf373c80b6d757db
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-04-04 22:40:01 -0300

    landingpage, bd

--- Equipe/LandingPage/assets/js/script.js
+++ Equipe/LandingPage/assets/js/script.js
@@ -1,13 +1,64 @@
-function change() {
-    var quemSomos = document.getElementById("Qs");
-    var valores = document.getElementById("V");
+function changeI() {
+    var inicio = document.getElementById("inicio");
+    var quemSomos = document.getElementById("quemSomos");
+    var valores = document.getElementById("valores");
+    var servicos = document.getElementById("servicos");
+    var equipe = document.getElementById("equipe");
+    inicio.style.display = "grid"
+    quemSomos.style.display = "none";
+    valores.style.display = "none";
+    servicos.style.display = "none";
+    equipe.style.display = "none";
+}
+
+function changeQ() {
+    var inicio = document.getElementById("inicio");
+    var quemSomos = document.getElementById("quemSomos");
+    var valores = document.getElementById("valores");
+    var servicos = document.getElementById("servicos");
+    var equipe = document.getElementById("equipe");
+    inicio.style.display = "none"
     quemSomos.style.display = "grid";
     valores.style.display = "none";
+    servicos.style.display = "none";
+    equipe.style.display = "none";
 }
 
 function changeV() {
-    var quemSomos = document.getElementById("Qs");
-    var valores = document.getElementById("V");
+    var inicio = document.getElementById("inicio");
+    var quemSomos = document.getElementById("quemSomos");
+    var valores = document.getElementById("valores");
+    var servicos = document.getElementById("servicos");
+    var equipe = document.getElementById("equipe");
+    inicio.style.display = "none"
     quemSomos.style.display = "none";
     valores.style.display = "grid";
+    servicos.style.display = "none";
+    equipe.style.display = "none";
+}
+
+function changeS() {
+    var inicio = document.getElementById("inicio");
+    var quemSomos = document.getElementById("quemSomos");
+    var valores = document.getElementById("valores");
+    var servicos = document.getElementById("servicos");
+    var equipe = document.getElementById("equipe");
+    inicio.style.display = "none"
+    quemSomos.style.display = "none";
+    valores.style.display = "none";
+    servicos.style.display = "grid";
+    equipe.style.display = "none";
+}
+
+function changeE() {
+    var inicio = document.getElementById("inicio");
+    var quemSomos = document.getElementById("quemSomos");
+    var valores = document.getElementById("valores");
+    var servicos = document.getElementById("servicos");
+    var equipe = document.getElementById("equipe");
+    inicio.style.display = "none"
+    quemSomos.style.display = "none";
+    valores.style.display = "none";
+    servicos.style.display = "none";
+    equipe.style.display = "grid";
 }

742d139e8f08b6fd3b0e6f5026fa9063e423c37d
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-05-20 21:01:13 -0300

    mergesort

--- TravelingMonkey/Site/controllers/cidadesController.js
+++ TravelingMonkey/Site/controllers/cidadesController.js
@@ -2,12 +2,47 @@ import express from "express";
 import Cidades from "../models/cidades.js";
 const router = express.Router();
 
-router.get('/cidades', function(req,res){
+// Função de Merge Sort
+function mergeSort(arr, key) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left, key), mergeSort(right, key), key);
+}
+
+function merge(left, right, key) {
+    let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+    // Ordenar os elementos comparando as chaves
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex][key] < right[rightIndex][key]) {
+            resultArray.push(left[leftIndex]);
+            leftIndex++; // move left array cursor
+        } else {
+            resultArray.push(right[rightIndex]);
+            rightIndex++; // move right array cursor
+        }
+    }
+
+    // Concatenar o resto dos elementos
+    return resultArray
+            .concat(left.slice(leftIndex))
+            .concat(right.slice(rightIndex));
+}
+
+router.get('/cidades', function(req, res) {
     Cidades.findAll().then(cidades => {
+        const sortedCidades = mergeSort(cidades, 'nome');
         res.render("cidades", {
-            cidades : cidades
-        })
-    })
+            cidades: sortedCidades
+        });
+    });
 });
+
 
 export default router;
